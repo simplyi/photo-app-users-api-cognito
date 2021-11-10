dc814ba376bb050744815b37deef22253884c3b8
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-11-10 13:28:23 -0500

    Initial commit

---
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/CreateUserHandler.java
@@ -0,0 +1,29 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+/**
+ * Handler for requests to Lambda function.
+ */
+public class CreateUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        return response
+                .withStatusCode(200)
+                .withBody("{}");
+
+    }
+
+}
---
+++ PhotoAppUsersAPICognito/src/test/java/com/appsdeveloperblog/aws/lambda/CreateUserHandlerTest.java
@@ -0,0 +1,23 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import com.appsdeveloperblog.aws.lambda.CreateUserHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class CreateUserHandlerTest {
+  @Test
+  public void successfulResponse() {
+//    CreateUserHandler app = new CreateUserHandler();
+//    APIGatewayProxyResponseEvent result = app.handleRequest(null, null);
+//    assertEquals(result.getStatusCode().intValue(), 200);
+//    assertEquals(result.getHeaders().get("Content-Type"), "application/json");
+//    String content = result.getBody();
+//    assertNotNull(content);
+//    assertTrue(content.contains("\"message\""));
+//    assertTrue(content.contains("\"hello world\""));
+//    assertTrue(content.contains("\"location\""));
+  }
+}
