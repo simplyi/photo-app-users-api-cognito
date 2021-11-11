fce4bacb2d86221da5bef3f7f9517a7fdd17f875
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-11-11 15:01:17 -0500

    Reading user details JSON from request body

--- PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/CreateUserHandler.java
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/CreateUserHandler.java
@@ -4,9 +4,12 @@ import java.util.HashMap;
 import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 
 /**
  * Handler for requests to Lambda function.
@@ -19,6 +22,12 @@ public class CreateUserHandler implements RequestHandler<APIGatewayProxyRequestE
 
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
                 .withHeaders(headers);
+
+        String requestBody = input.getBody();
+        LambdaLogger logger  = context.getLogger();
+        logger.log("Original json body: " +  requestBody);
+
+        JsonObject userdetails = JsonParser.parseString(requestBody).getAsJsonObject();
 
         return response
                 .withStatusCode(200)
