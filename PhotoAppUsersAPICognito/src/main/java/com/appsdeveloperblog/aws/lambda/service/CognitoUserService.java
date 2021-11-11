4f224281ac0f50896f3a89cea4d25df4221fb595
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-11-11 15:01:39 -0500

    Created a new CognitoUserService class

---
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.aws.lambda.service;
+
+import com.google.gson.JsonObject;
+
+public class CognitoUserService {
+
+    public JsonObject createUser(JsonObject user) {
+
+        JsonObject storedUserDetails = new JsonObject();
+        return storedUserDetails;
+    }
+
+}
