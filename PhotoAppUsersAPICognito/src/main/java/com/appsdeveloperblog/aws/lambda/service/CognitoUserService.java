14bf14f3390ceac3a03f595f0732b399db427ea7
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-11-12 13:23:31 -0500

    Implemented createUser() method in the CognitoUserService class

--- PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/service/CognitoUserService.java
@@ -1,13 +1,96 @@
 package com.appsdeveloperblog.aws.lambda.service;
 
 import com.google.gson.JsonObject;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.AttributeType;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpRequest;
+import software.amazon.awssdk.services.cognitoidentityprovider.model.SignUpResponse;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
+import java.util.UUID;
 
 public class CognitoUserService {
+    private final CognitoIdentityProviderClient cognitoIdentityProviderClient;
 
-    public JsonObject createUser(JsonObject user) {
+    public CognitoUserService(String region) {
+        this.cognitoIdentityProviderClient = CognitoIdentityProviderClient.builder()
+                .region(Region.of(region))
+                .build();
+    }
 
-        JsonObject storedUserDetails = new JsonObject();
-        return storedUserDetails;
+    public CognitoUserService(CognitoIdentityProviderClient cognitoIdentityProviderClient) {
+        this.cognitoIdentityProviderClient = cognitoIdentityProviderClient;
+    }
+
+    public JsonObject createUser(JsonObject user,
+                                 String appClientId, String appClientSecret) {
+        String email = user.get("email").getAsString();
+        String password = user.get("password").getAsString();
+        String userId = UUID.randomUUID().toString();
+        String firstName = user.get("firstName").getAsString();
+        String lastName = user.get("lastName").getAsString();
+
+        AttributeType emailAttribute = AttributeType.builder()
+                .name("email")
+                .value(email)
+                .build();
+
+        AttributeType nameAttribute = AttributeType.builder()
+                .name("name")
+                .value(firstName + "  " + lastName)
+                .build();
+
+        AttributeType userIdAttribute = AttributeType.builder()
+                .name("custom:userId")
+                .value(userId)
+                .build();
+
+        List<AttributeType> attributes = new ArrayList<>();
+        attributes.add(emailAttribute);
+        attributes.add(nameAttribute);
+        attributes.add(userIdAttribute);
+
+        String generatedSecretHash = calculateSecretHash(appClientId, appClientSecret, email);
+
+        SignUpRequest signUpRequest = SignUpRequest.builder()
+                .username(email)
+                .password(password)
+                .userAttributes(attributes)
+                .clientId(appClientId)
+                .secretHash(generatedSecretHash)
+                .build();
+
+        SignUpResponse signupResponse = cognitoIdentityProviderClient.signUp(signUpRequest);
+        JsonObject createUserResult = new JsonObject();
+        createUserResult.addProperty("isSuccessful", signupResponse.sdkHttpResponse().isSuccessful());
+        createUserResult.addProperty("statusCode", signupResponse.sdkHttpResponse().statusCode());
+        createUserResult.addProperty("cognitoUserId", signupResponse.userSub());
+        createUserResult.addProperty("isConfirmed", signupResponse.userConfirmed());
+        
+        return createUserResult;
+    }
+
+    public String calculateSecretHash(String userPoolClientId, String userPoolClientSecret, String userName) {
+        final String HMAC_SHA256_ALGORITHM = "HmacSHA256";
+
+        SecretKeySpec signingKey = new SecretKeySpec(
+                userPoolClientSecret.getBytes(StandardCharsets.UTF_8),
+                HMAC_SHA256_ALGORITHM);
+        try {
+            Mac mac = Mac.getInstance(HMAC_SHA256_ALGORITHM);
+            mac.init(signingKey);
+            mac.update(userName.getBytes(StandardCharsets.UTF_8));
+            byte[] rawHmac = mac.doFinal(userPoolClientId.getBytes(StandardCharsets.UTF_8));
+            return Base64.getEncoder().encodeToString(rawHmac);
+        } catch (Exception e) {
+            throw new RuntimeException("Error while calculating ");
+        }
     }
 
 }
