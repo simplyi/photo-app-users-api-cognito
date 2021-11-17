b1dfb38863893b0ca0a7693db0827839a2063dfc
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-11-17 09:41:05 -0500

    Created LoginUserHandler class

---
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/LoginUserHandler.java
@@ -0,0 +1,22 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class LoginUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+                .withHeaders(headers);
+
+        return response;
+    }
+}
