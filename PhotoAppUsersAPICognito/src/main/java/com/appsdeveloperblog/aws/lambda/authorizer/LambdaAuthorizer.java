9587a9b7118e37d470fbf22e904f4919e52bc48b
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-11-28 10:20:54 -0500

    Created a new lambda function called LambdaAuthorizer

---
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/authorizer/LambdaAuthorizer.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.aws.lambda.authorizer;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        return new APIGatewayProxyResponseEvent();
+    }
+}
