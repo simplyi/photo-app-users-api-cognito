f376496034d40b2501c9866ac83b816a84c81607
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-11-29 09:36:41 -0500

    Created a new function GetUserByUsernameHandler

---
+++ PhotoAppUsersAPICognito/src/main/java/com/appsdeveloperblog/aws/lambda/GetUserByUsernameHandler.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.aws.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class GetUserByUsernameHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        return new APIGatewayProxyResponseEvent();
+    }
+}
