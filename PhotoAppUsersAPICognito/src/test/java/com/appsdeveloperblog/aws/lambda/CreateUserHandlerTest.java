c14172de4b6465c1dcf7b2f74e8a127f3f0cd467
Sergey Kargopolov <sergey.kargopolov@gmail.com>
2021-12-23 17:58:58 -0500

    Lifecycle methods

--- PhotoAppUsersAPICognito/src/test/java/com/appsdeveloperblog/aws/lambda/CreateUserHandlerTest.java
+++ PhotoAppUsersAPICognito/src/test/java/com/appsdeveloperblog/aws/lambda/CreateUserHandlerTest.java
@@ -1,10 +1,30 @@
 package com.appsdeveloperblog.aws.lambda;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class CreateUserHandlerTest {
+
+    @BeforeEach
+    public void runBeforeEachTestMethod() {
+        System.out.println("Executing @BeforeEach method");
+    }
+
+    @AfterEach
+    public void runAfterEachTestMethod() {
+       System.out.println("Executing @AfterEach method");
+    }
+
+    @BeforeAll
+    public static void runBeforeAllTestMethods()  {
+        System.out.println("Executing @BeforeAll method");
+    }
+
+    @AfterAll
+    public static void runAfterAllTestMethods() {
+        System.out.println("Executing @AfterAll method");
+    }
 
     @Test
     public void testHandleRequest_whenValidDetailsProvided_returnsSuccessfulResponse() {
